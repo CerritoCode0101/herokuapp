08bc557a5cbbd171674760cc3c0ccf161ceae003
CerritoCode0101 <CaseyCerrito@gmail.com>
2021-02-11 12:51:21 -0600

    deploy

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
-const dotenv = require("dotenv").config()
-const express = require("express")
+const dotenv = require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const path = require("path")
 
-const app = express()
-const port = process.env.PORT || 9000
+const app = express();
+const port = process.env.PORT || 9000;
 
-app.use("/api/*", (_,res)=>{
-    res.json({data:"The API LIVES!!"})
-})
+app.use(cors());
+app.use(express.json());
+app.use(express.static(path.join(__dirname, "client/build", "index.html")))
 
-app.listen(port,() => {
-console.log(`server is alive on port ${port}`)})
+app.use("/api/*", (_, res) => {
+  res.json({ data: "The API LIVES!!" });
+});
+
+app.listen(port, () => {
+  console.log(`server is alive on port ${port}`);
+});
 
